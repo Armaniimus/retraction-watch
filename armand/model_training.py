51e3b18ab9a562cf49b7ac225c21b4c308eee715
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2025-10-29 22:51:09 +0100

    fixed a few bugs in my class

--- armand/model_training.py
+++ armand/model_training.py
@@ -4,9 +4,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 class ML_Model_Builder:
-	def __init__(self, dataset: pd.DataFrame, target: pd.DataFrame, randomState:int = 42):
+	def __init__(self, dataset: pd.DataFrame, target: pd.DataFrame, random_state:int = 42):
 		self.__model = None
-		self.randomState = randomState
+		self.random_state = random_state
 		
 		# First split: Separate the test set from the rest
 		x_train_val, test_x, y_train_val, test_y = train_test_split(
@@ -32,10 +32,6 @@ class ML_Model_Builder:
 
 		self.__data_val_x = val_x
 		self.__target_val_y = val_y
-
-		
-		
-		return self		
 	
 	def setModel(self, model = "decision_tree"):
 		if (model == "decision_tree"):
@@ -46,15 +42,15 @@ class ML_Model_Builder:
 		return self
 	
 	def build(self):
-		if self.__data_train_x == None or self.__target_train_y == None:
+		if self.__data_train_x is None or self.__target_train_y is None:
 			error = f"training data is not loaded is x_train valid:{self.__data_train_x==None} is y_train valid:{self.__target_train_y==None}"
 			raise Exception(error)
 		
-		if self.__data_test_x == None or self.__target_test_y == None:
+		if self.__data_test_x is None or self.__target_test_y is None:
 			error = f"testdata is not loaded, is x_test valid:{self.__data_test_x==None}, is y_test valid:{self.__target_test_y==None}"
 			raise Exception(error)
 		
-		if self.__data_val_x == None or self.__target_val_y == None:
+		if self.__data_val_x is None or self.__target_val_y is None:
 			error = f"validationdata is not loaded, is x_val valid:{self.__data_val_x==None}, is y_val valid:{self.__target_val_y==None}"
 			raise Exception(error)
 		
@@ -66,18 +62,21 @@ class ML_Model_Builder:
 			raise Exception(f"invalid or not supported model is set")
 
 class Decision_Tree_Model:
-	def __init__(self, x_train, y_train, x_test, y_test, x_val, y_val):
+	def __init__(self, x_train, y_train, x_test, y_test, x_val, y_val, random_state = 42):
 		self.__x_train = x_train
 		self.__y_train = y_train
 		self.__x_test = x_test
 		self.__y_test = y_test
 		self.__x_val = x_val
 		self.__y_val = y_val
+		self.__random_state = random_state
 
+	def setRandomstate(self, random_state=42):
+		self.__random_state = random_state
 		return self
 
-	def train(self, random_state:int=42):
-		self.__model = DecisionTreeClassifier(random_state=random_state)
+	def train(self):
+		self.__model = DecisionTreeClassifier(random_state=self.__random_state)
 		self.__model.fit(self.__x_train, self.__y_train)
 		return self	
 	
