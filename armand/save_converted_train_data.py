745d98b9adff0527e441a99f258fe7f3b97c96e4
Armaniimus l337lap-23 <tommy_chester@hotmail.com>
2025-10-30 12:35:50 +0100

    data preperation that didn't throw errors

--- armand/save_converted_train_data.py
+++ armand/save_converted_train_data.py
@@ -7,18 +7,17 @@ def save_data():
 	df = df[['Subject', 'Country', 'ArticleType', 'RetractionDate', 'OriginalPaperDate', 'RetractionNature', 'Reason', 'Paywalled' ]]
 	data = context_aware_add_counted_dates(df, "days_in_between")
 
-	train_set = allHotencoding(data, ['RetractionNature', 'Paywalled'], ["Subject", "Reason", "Country", "ArticleType"], ";")
-	train_set = addIn2022(train_set)
+	train_set= allHotencoding(data, ['RetractionNature', 'Paywalled'], ["Subject", "Reason", "Country", "ArticleType"], ";")
+	target_set = addIn2022(train_set.copy())
+	target_set = target_set["in2022"]
       
-	train_set['RetractionDate'] = robust_date_to_timestamp(train_set['RetractionDate'])
 	train_set['OriginalPaperDate'] = robust_date_to_timestamp(train_set['OriginalPaperDate'])
-	target_set = train_set["in2022"]
+	train_set = train_set.drop(columns=['RetractionDate'])
 
 	save_csv(train_set,  "data/train_set.csv")
 	save_csv(target_set, "data/target_set.csv")
 
 def robust_date_to_timestamp(series: pd.Series) -> pd.Series:
-    # ... (the function we developed in the previous step) ...
     numeric_series = pd.to_numeric(series, errors='coerce')
     is_date_string = numeric_series.isna()
     datetime_converted = (pd.to_datetime(series.loc[is_date_string]).astype('int64') // 10**9)
